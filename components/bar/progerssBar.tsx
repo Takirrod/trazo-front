710bad7261ef5ead626bd6347dd39f5a6aff97ef
Jarrod <rodrigoquiroz308@gmail.com>
2022-12-16 09:08:32 -0400

    fix currentstep negative add

--- components/bar/progerssBar.tsx
+++ components/bar/progerssBar.tsx
@@ -35,7 +35,7 @@ function ProgressBar({
   } else {
     const trazo = stepsNumber as TrazoHome;
 
-    currentStep = currentStep >= 0 ? trazo.pasoActual : 0;
+    currentStep = currentStep > 0 ? trazo.pasoActual : 0;
 
     for (let i = 0; i < trazo.cantidadPasos; i++) {
       steps.push({
